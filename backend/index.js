9bf2464d271bfa36258cc9950768c15730a34b16
Rishit-Thaper <rishitthaper5329@gmail.com>
2023-07-21 13:12:23 +0530

    env publish

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,13 @@ const cors = require('cors')
 app.use(express.json());
 
 app.use(cors())
-mongoose.connect()
+mongoose.connect(process.env.MONGODB_URI,{UseNewUrlParser: true, UseUnifiedTopology: true})
+    .then(()=>{
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server Started at Port No. ${process.env.PORT}`)
+        })
+        console.log(`Database Connected ${process.env.MONGODB_URI}`)
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
